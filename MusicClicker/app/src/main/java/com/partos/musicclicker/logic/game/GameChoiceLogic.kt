562ec02eafa0a22c25738fcc963eecabe47a1352
Papryk <gustaw5022@gmail.com>
2020-07-23 17:30:49 +0200

    created layout

---
+++ MusicClicker/app/src/main/java/com/partos/musicclicker/logic/game/GameChoiceLogic.kt
@@ -0,0 +1,11 @@
+package com.partos.musicclicker.logic.game
+
+import android.view.View
+import androidx.fragment.app.FragmentManager
+
+class GameChoiceLogic (val rootView: View, val fragmentManager: FragmentManager) {
+
+    fun initFragment() {
+
+    }
+}
