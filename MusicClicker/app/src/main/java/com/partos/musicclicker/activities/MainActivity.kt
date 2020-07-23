1c891c8c1d4da5a14000e9567089fb92fc56f8d9
Papryk <gustaw5022@gmail.com>
2020-07-23 13:47:33 +0200

    added background music

--- MusicClicker/app/src/main/java/com/partos/musicclicker/activities/MainActivity.kt
+++ MusicClicker/app/src/main/java/com/partos/musicclicker/activities/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.partos.musicclicker.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.partos.musicclicker.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.background)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+    }
+
+    override fun onPause() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause()
+        }
+        super.onPause()
+    }
+
+    override fun onResume() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start()
+        }
+        super.onResume()
     }
 }
